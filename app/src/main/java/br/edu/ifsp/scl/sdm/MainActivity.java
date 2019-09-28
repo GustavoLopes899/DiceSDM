cdb484ee4d804110276c72f02a2199bfce7329f4
Gustavo Lopes <GustavoLopes899@gmail.com>
2019-09-28 11:19:03 -0300

    Implementando interface de click. Inicializando os atributos da classe.

--- app/src/main/java/br/edu/ifsp/scl/sdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/MainActivity.java
@@ -1,14 +1,14 @@
 package br.edu.ifsp.scl.sdm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     // Random usado para simular o lançamento do dado
     private Random geradorRandomico;
@@ -22,5 +22,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Após a criação da tela
+        geradorRandomico = new Random(System.currentTimeMillis());
+
+        // Recuperando referência para o resultadoTextView do arquivo de layout
+        resultadoTextView = findViewById(R.id.resultadoTextView);
+
+        // Recuperando referência para o jogarDadoButton do arquivo de layout
+        jogarDadoButton = findViewById(R.id.jogarDadoButton);
+        jogarDadoButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.jogarDadoButton) {
+            int resultado = geradorRandomico.nextInt(6) + 1;
+            resultadoTextView.setText(getString(R.string.face_sorteada) + resultado);
+        }
+    }
+
 }
